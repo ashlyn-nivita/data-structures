e4f616b7a34f0a0cf9b978b7a63f8db73daaffe1
ashlyn-nivita <34641381+ashlyn-nivita@users.noreply.github.com>
2018-02-10 09:29:05 +0530

    Create linkedlist_userinput.c

---
+++ linkedlist_userinput.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct n
+{
+    int data;
+    struct n *next;
+};
+typedef struct n node;
+ node *head,*first,*second,*third,*temp;
+int display(int);
+int main()
+{
+    int n1,n2,n3,n4;
+    head=(node*)malloc(sizeof(node));
+    first=(node*)malloc(sizeof(node));
+    second=(node*)malloc(sizeof(node));
+    third=(node*)malloc(sizeof(node));
+    scanf("%d%d%d%d",&n1,&n2,&n3,&n4);
+    head->data=n1;
+    head->next=first;
+    first->data=n2;
+    first->next=second;
+    second->data=n3;
+    second->next=third;
+    third->data=n4;
+    third->next=NULL;
+    temp=head;
+    display(head);
+}
+int display(int head)
+{
+    while(temp!=NULL)
+    {
+        printf("%d\n",temp->data);
+        temp=temp->next;
+    }
+}
